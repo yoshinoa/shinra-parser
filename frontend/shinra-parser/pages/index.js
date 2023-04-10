8bbbd8a258d771bed7097e06284be344efd085c1
Alexander Yoshino <alexander.yoshino@mail.utoronto.ca>
2023-04-10 06:16:51 -0400

    aesthetic changes

--- frontend/shinra-parser/pages/index.js
+++ frontend/shinra-parser/pages/index.js
@@ -1,18 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { auth } from "@/firebase/firebase";
 import { useRouter } from "next/navigation";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
 
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Lumber!</h1>
-        <p className={styles.description}>Sign in to get started</p>
-      </main>
-    </div>
-  );
+  useEffect(() => {
+    if (auth.currentUser) {
+      router.push("/home");
+    } else {
+      router.push("/login");
+    }
+  }, []);
+
+  return <div className={styles.container}></div>;
 }
